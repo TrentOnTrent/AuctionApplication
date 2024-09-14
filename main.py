84a36bba73be0f00ace4a89067dc3d30fec3bc06
TrentOnTrent <Trentondunstan@hotmail.com>
2024-09-14 13:23:36 +1000

    Initialising flaskenv and init/main files

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask
+
+from init import db, ma, bcrypt, jwt
+
+def create_app():
+    app = Flask(__name__)
+    app.json.sort_keys = False
+    # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    # app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    @app.errorhandler(ValidationError)
+    def validation_error(err):
+        return {"error": err.messages}, 400
+    
+    @app.errorhandler(400)
+    def bad_request(err):
+        return {"error": err.messages}, 400
+    
+    @app.errorhandler(401)
+    def unauthorised():
+        return {"error": "You are not an authorised user."}, 401
+
+    # app.register_blueprint(db_commands)
+    # app.register_blueprint(auth_bp)
+    # app.register_blueprint(cards_bp)
+    return app
