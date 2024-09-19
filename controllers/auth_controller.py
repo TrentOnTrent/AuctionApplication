e6d6a49fe7b22233e5ef973215a30041fd41fb16
TrentOnTrent <Trentondunstan@hotmail.com>
2024-09-19 17:44:30 +1000

    Added integrity error checking

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,27 +1,33 @@
 from flask import Blueprint, request
 from models.user import User, user_schema
 from init import bcrypt, db
+from sqlalchemy.exc import IntegrityError
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 @auth_bp.route("/register", methods=["POST"])
 def register_user():
-    body = request.get_json()
+    try:
+        body = request.get_json()
 
-    user = User(
-        email = body.get("email"),
-        username = body.get("username"),
-    )
+        user = User(
+            email = body.get("email"),
+            username = body.get("username"),
+        )
 
-    password = body.get("password")
-    if password:
-        user.password = bcrypt.generate_password_hash(password).decode("utf-8")
+        password = body.get("password")
+        if password:
+            user.password = bcrypt.generate_password_hash(password).decode("utf-8")
 
-    db.session.add(user)
-    db.session.commit()
+        db.session.add(user)
+        db.session.commit()
 
-    return user_schema.dump(user), 200
+        return user_schema.dump(user), 200
+    except IntegrityError:
+        return {"Error": "Incorrect input"}, 400
 
 @auth_bp.route("/login")
 def login_user():
-    pass
+    body = request.get_json()
+
+    
