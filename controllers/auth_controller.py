4f596c1b9e9a5f59e07cad3aa57bd2a0fb8af211
TrentOnTrent <Trentondunstan@hotmail.com>
2024-09-19 20:58:01 +1000

    Added login route

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -2,6 +2,9 @@ from flask import Blueprint, request
 from models.user import User, user_schema
 from init import bcrypt, db
 from sqlalchemy.exc import IntegrityError
+from psycopg2 import errorcodes
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
@@ -23,11 +26,22 @@ def register_user():
         db.session.commit()
 
         return user_schema.dump(user), 200
-    except IntegrityError:
-        return {"Error": "Incorrect input"}, 400
+    
+    except IntegrityError as err:
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            return {"Error": f"Missing required field: {err.orig.diag.column_name}"}, 400
+        
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+            return {"Error": f"Field not unique"}, 400
 
-@auth_bp.route("/login")
+@auth_bp.route("/login", methods=["POST"])
 def login_user():
     body = request.get_json()
+    stmt = db.select(User).filter_by(email=body.get("email"))
+    user = db.session.scalar(stmt)
 
-    
+    if user and bcrypt.check_password_hash(user.password, body.get("password")):
+        token = create_access_token(identity=str(user.id),expires_delta=timedelta(days=1))
+        return {"email": user.email, "username": user.username, "token": token}
+    else:
+        return {"error": "Invalid email or password"}, 400
